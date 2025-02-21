c1956742d73b06d7a1f7e2278649fc4e5c504948
TedyPermana24 <tedysukma24@gmail.com>
2025-02-21 14:08:38 +0700

    add articles page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 import { Navbar } from "@/components/molecules";
 import { AuroraHero } from "@/components/organism/hero";
+import { ArticleCard } from "@/components/articles/card";
+import { Articles } from '@/data/articles';
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
     <div className="bg-gray-950">
       <Navbar/>
       <AuroraHero/>
-      <div className="h-screen">
-        Latar belakang
+      <div className="container mx-auto px-4 py-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {Articles.map((article, index) => (
+            <Link href={`/articles/${article.slug}`} key={index}>
+              <ArticleCard
+                title={article.title}
+                description={article.description}
+                image={article.image}
+                date={article.date}
+                slug={article.slug}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
